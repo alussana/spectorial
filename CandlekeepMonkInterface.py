bcc17052d59d35e81d80dd9a8cc1120595733251
alussana <alessandro.lussana@protonmail.com>
2020-08-01 22:37:13 +0200

    pushed first functioning version

---
+++ CandlekeepMonkInterface.py
@@ -0,0 +1,70 @@
+import CandlekeepMonkSkills
+
+def candlekeepMonk(tomesAreOpen = None):
+    if tomesAreOpen == None:
+        print("\n Candlekeep Monk:")
+        print(" \"Greetings. Just one moment and I'll help you with your quest.\"")
+        print(" *Starts looking for some tomes summoning the Internet planar portal*")
+        tomesAreOpen = CandlekeepMonkSkills.openTheTomes()
+        print("\n Candlekeep Monk:")
+        if tomesAreOpen == False:
+            print(" \"My apoligies. It seems that I cannot retrieve the books you are looking for.\"")
+            print(" \"Did you check your internet connection?\"\n")
+            exit()
+        else:
+            print(" \"Here we are. What kind of knowledge are you searching for?\"\n")
+    else:
+        print("\n Candlekeep Monk:")
+        print(" \"Do you need something else?\"")
+        answerInput = input("\n --> [y/n]: ")
+        if answerInput != 'y' and answerInput != 'yes':
+            print("\n Candlekeep Monk:")
+            print(" \"Farewell.\"\n")
+            exit()
+        print("\n Candlekeep Monk:")
+        print(" \"How may I assist you?\"\n")
+    objType = ""
+    indexesData, indexesDict, objDictPprint = CandlekeepMonkSkills.listKnowledge(objType)
+    print(objDictPprint)
+    objTypeInput = input("\n --> : ")
+    objType = indexesDict[int(objTypeInput)]
+    indexesData, indexesDict, objDictPprint = CandlekeepMonkSkills.listKnowledge(objType)
+    indexesData_count = int(indexesData["count"])
+    print("\n Candlekeep Monk:")
+    if indexesData_count <= 40:
+        print(f" \"Very well, {objType}. Here is what I have.\"\n")
+        print(objDictPprint)
+    else:
+        print(f" \"Let me see... I've got {indexesData_count} results for {objType}.")
+        print(" Do you know its name already? If not, try to use the first letters only.\"")
+    objNameInput = input("\n --> : ")
+    while type(objNameInput) != int:
+        try:
+            objNameInput = int(objNameInput)
+        except:
+            indexesDataRefinedPprint = CandlekeepMonkSkills.searchKnowledge(indexesData, objNameInput)
+            print()
+            print(indexesDataRefinedPprint)
+            objNameInput = input("\n --> : ")
+    objName = indexesDict[objNameInput]
+    objIndex = CandlekeepMonkSkills.findObjIndex(objType, objName)
+    objData, objDataPprint = CandlekeepMonkSkills.getObjData(objType, objIndex)
+    print("\n Candlekeep Monk:")
+    print(" \"Here is what you asked for. Use it wisely.\"\n")
+    print("     _______________________________________________________________________________________")
+    print("    /\                                                                                      \\")
+    print("(O)===)><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><)==(O)")
+    print("    \/______________________________________________________________________________________/")
+    print()
+    toprint = objDataPprint.split("\n")
+    for line in toprint:
+        print("      " + line)
+    print("     _______________________________________________________________________________________")
+    print("    /\                                                                                      \\")
+    print("(O)===)><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><)==(O)")
+    print("    \/______________________________________________________________________________________/")
+    print()
+    candlekeepMonk(tomesAreOpen)
+
+if __name__ == '__main__':
+    candlekeepMonk()
---
+++ CandlekeepMonkSkills.py
@@ -0,0 +1,105 @@
+import urllib.request
+import os
+import json
+import pprint
+
+dndapiurl = "https://www.dnd5eapi.co/api/"
+
+def downloadObjData(objType, objIndex):
+    os.makedirs(objType, exist_ok=True)
+    objFileName = objType + "/" + objIndex
+    urllib.request.urlretrieve(dndapiurl + objFileName, objFileName)
+    objFile = open(objFileName)
+    objTextData = objFile.read()
+    objFile.close()
+    objData = json.loads(objTextData)
+    objFile = open(objFileName, "w")
+    print(json.dumps(objData, sort_keys=True, indent=4), file=objFile)
+    objFile.close()
+
+def downloadIndexes(objType):
+    os.makedirs("indexes", exist_ok=True)
+    indexesFileName =  "indexes/" + objType
+    urllib.request.urlretrieve(dndapiurl + objType + "/", indexesFileName)
+    indexesFile = open(indexesFileName)
+    indexesTextData = indexesFile.read()
+    indexesFile.close()
+    indexesData = json.loads(indexesTextData)
+    indexesFile = open(indexesFileName, "w")
+    print(json.dumps(indexesData, sort_keys=True, indent=4), file=indexesFile)
+    indexesFile.close()
+
+def getIndexes(objType):
+    indexesFileName =  "indexes/" + objType
+    if os.path.isfile(indexesFileName) == False:
+        downloadIndexes(objType)
+    indexesFile = open(indexesFileName)
+    indexesTextData = indexesFile.read()
+    indexesFile.close()
+    indexesData = json.loads(indexesTextData)
+    return(indexesData)
+    
+def findObjIndex(objType, objName):
+    indexesFileName =  "indexes/" + objType
+    if os.path.isfile(indexesFileName) == False:
+        downloadIndexes(objType)
+    indexesFile = open(indexesFileName)
+    indexesTextData = indexesFile.read()
+    indexesFile.close()
+    indexesData = json.loads(indexesTextData)
+    for entry in indexesData["results"]:
+        if entry["name"] == objName:
+            objIndex = entry["index"]
+            return(objIndex)
+
+def getObjData(objType, objIndex):
+    objFileName = objType + "/" + objIndex
+    if os.path.isfile(objFileName) == False:
+       downloadObjData(objType, objIndex)
+    objFile = open(objFileName)
+    objTextData = objFile.read()
+    objFile.close()
+    objData = json.loads(objTextData)
+    return(objData, pprint.pformat(objData))
+
+def openTheTomes():
+    os.makedirs("indexes", exist_ok=True)
+    indexesFileName =  "indexes/api"
+    try:
+        urllib.request.urlretrieve(dndapiurl, indexesFileName)
+        indexesFile = open(indexesFileName)
+        indexesTextData = indexesFile.read()
+        indexesFile.close()
+        indexesData = json.loads(indexesTextData)
+        indexesFile = open(indexesFileName, "w")
+        print(json.dumps(indexesData, sort_keys=True, indent=4), file=indexesFile)
+        indexesFile.close()
+        return(True)
+    except:
+        return(False)
+
+def listKnowledge(objType):
+    if objType == "":
+        objType = "api"
+    indexesData = getIndexes(objType)
+    indexesDict = {}
+    i = 0
+    if objType == "api":
+        for key in indexesData.keys():
+            indexesDict[i] = key
+            i = i + 1
+    else:
+        for entry in indexesData["results"]:
+            indexesDict[i] = entry["name"]
+            i = i + 1
+    return(indexesData, indexesDict, pprint.pformat(indexesDict))
+
+def searchKnowledge(indexesData, objName):
+    indexesDict = {}
+    i = 0
+    for entry in indexesData["results"]:
+        if entry["name"].startswith(objName) or entry["index"].startswith(objName):
+            indexesDict[i] = entry["name"]
+        i = i + 1
+    return(pprint.pformat(indexesDict))
+    
